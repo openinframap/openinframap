a43d5932baf0579ff1ea1438c78b3ea0ad375e85
Russ Garrett <russ@garrett.co.uk>
2018-10-15 23:07:00 +0100

    Babel blah

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,20 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
+  module: {
+  rules: [
+    {
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }
+  ]
+},
   plugins: [
     new UglifyJSPlugin(),
     new webpack.DefinePlugin({
