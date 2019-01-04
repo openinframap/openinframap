94642528d206eb2986911e69fd374b187400c484
Russ Garrett <russ@garrett.co.uk>
2019-01-04 16:43:20 +0000

    Styles updates

--- functions.sql
+++ functions.sql
@@ -100,3 +100,22 @@ BEGIN
     END IF;
 END
 $$ LANGUAGE plpgsql;
+
+
+-- Rendering functions
+
+create or replace function ZRes (z integer)
+    returns float
+    returns null on null input
+    language sql immutable as
+$func$
+select (40075016.6855785/(256*2^z));
+$func$;
+
+create or replace function ZRes (z float)
+    returns float
+    returns null on null input
+    language sql immutable as
+$func$
+select (40075016.6855785/(256*2^z));
+$func$;
