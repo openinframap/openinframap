ac1f96b43c7be8b947fb95f616f6c1b7a404bbf0
Russ Garrett <russ@garrett.co.uk>
2020-11-10 09:41:17 +0000

    Dependencies update

--- webpack.common.js
+++ webpack.common.js
@@ -1,33 +1,33 @@
-const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const SpritezeroWebpackPlugin = require('spritezero-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const SpritezeroWebpackPlugin = require("spritezero-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist")
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
+        use: ["style-loader", "css-loader"]
+      }
     ],
-    noParse: /(mapbox-gl)\.js$/,
+    noParse: /(mapbox-gl)\.js$/
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.html',
+      template: "src/index.html"
     }),
     new SpritezeroWebpackPlugin({
-      source: 'sprites/*.svg'
+      source: "sprites/*.svg"
     }),
-    new CopyPlugin([
-      {from: 'sprites', to: 'style/sprites'}
-    ]),
-  ],
+    new CopyPlugin({
+      patterns: [{ from: "sprites", to: "style/sprites" }]
+    })
+  ]
 };
