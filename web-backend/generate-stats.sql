67630dd44c88f095bfa3ff1b32d785ce8a7d4eac
Russ Garrett <russ@garrett.co.uk>
2024-08-01 13:22:11 +0100

    Update dependencies, switch to ruff, add test job

--- web-backend/generate-stats.sql
+++ web-backend/generate-stats.sql
@@ -31,28 +31,28 @@ CREATE TABLE stats.power_generator (
 
 
 INSERT INTO stats.power_line (time, country, voltage, length)
-	SELECT now() AS time, country_eez."union" AS country, convert_voltage(voltage)::INTEGER AS voltage,
+	SELECT now() AS time, country_eez_sub."union" AS country, convert_voltage(voltage)::INTEGER AS voltage,
 		sum(ST_Length(ST_Transform(geometry, 4326)::geography)) AS length
-	FROM osm_power_line, countries.country_eez
-	WHERE ST_Contains(ST_Transform(country_eez.geom, 3857), geometry)
+	FROM osm_power_line, countries.country_eez_sub
+	WHERE ST_Contains(country_eez_sub.geom, geometry)
 		AND "union" != 'Antarctica'
 		AND tags->'construction:power' IS NULL
-	GROUP BY country_eez."union", convert_voltage(voltage);
+	GROUP BY country_eez_sub."union", convert_voltage(voltage);
 
 
 INSERT INTO stats.power_plant (time, country, source, count, output)
-	SELECT now() AS time, country_eez."union", source, count(*), convert_power(output)
-	FROM power_plant, countries.country_eez
-	WHERE ST_Contains(ST_Transform(country_eez.geom, 3857), geometry)
+	SELECT now() AS time, country_eez_sub."union", source, count(*), convert_power(output)
+	FROM power_plant, countries.country_eez_sub
+	WHERE ST_Contains(country_eez_sub.geom, geometry)
 		AND "union" != 'Antarctica'
 		AND tags->'construction:power' IS NULL
-	GROUP BY country_eez."union", source, convert_power(output);
+	GROUP BY country_eez_sub."union", source, convert_power(output);
 
 
 INSERT INTO stats.power_generator (time, country, source, count, output)
-	SELECT now() AS time, country_eez."union", source, count(*), convert_power(output)
-	FROM osm_power_generator, countries.country_eez
-	WHERE ST_Contains(ST_Transform(country_eez.geom, 3857), geometry)
+	SELECT now() AS time, country_eez_sub."union", source, count(*), convert_power(output)
+	FROM osm_power_generator, countries.country_eez_sub
+	WHERE ST_Contains(country_eez_sub.geom, geometry)
 		AND "union" != 'Antarctica'
 		AND tags->'construction:power' IS NULL
-	GROUP BY country_eez."union", source, convert_power(output);
+	GROUP BY country_eez_sub."union", source, convert_power(output);
