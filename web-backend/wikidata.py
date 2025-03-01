9b3c5d8c392becec25a5c4989aae8c70ce297d4e
Russ Garrett <russ@garrett.co.uk>
2025-03-01 18:37:17 +0000

    Refactor and add "Part of" data

--- web-backend/wikidata.py
+++ web-backend/wikidata.py
@@ -1,7 +1,9 @@
 """ Endpoints to proxy WikiData requests for info popups on the map """
 
+from typing import Optional
+import httpx
 from starlette.exceptions import HTTPException
-from starlette.responses import JSONResponse
+from starlette.responses import JSONResponse, Response
 from data import get_wikidata, get_commons_thumbnail
 from util import cache_for
 from main import app
@@ -9,16 +11,34 @@ from main import app
 
 @app.route("/wikidata/{wikidata_id}")
 @cache_for(86400)
-async def wikidata(request):
+async def wikidata(request) -> Response:
     wikidata_id = request.path_params["wikidata_id"].upper()
     http_client = request.state.http_client
 
-    response = {}
-    data = await get_wikidata(wikidata_id, http_client)
-    if data is None:
+    response = await wikidata_json(wikidata_id, http_client)
+    if response is None:
         raise HTTPException(404, "Wikidata item not found")
 
+    return JSONResponse(
+        response,
+        headers={"Access-Control-Allow-Origin": "*"},
+    )
+
+
+async def wikidata_json(
+    wikidata_id: str, http_client: httpx.AsyncClient
+) -> Optional[dict]:
+    data = await get_wikidata(wikidata_id, http_client)
+    if data is None:
+        return None
+
+    response = {}
+    response["labels"] = {
+        label["language"]: label["value"] for label in data["labels"].values()
+    }
+
     response["sitelinks"] = data["sitelinks"]
+
     if (
         "P18" in data["claims"]
         and data["claims"]["P18"][0]["mainsnak"]["datatype"] == "commonsMedia"
@@ -35,7 +55,13 @@ async def wikidata(request):
             "value"
         ]
 
-    return JSONResponse(
-        response,
-        headers={"Access-Control-Allow-Origin": "*"},
-    )
+    if "P361" in data["claims"]:
+        response["part_of"] = []
+        for claim in data["claims"]["P361"]:
+            response["part_of"].append(
+                await wikidata_json(
+                    claim["mainsnak"]["datavalue"]["value"]["id"], http_client
+                )
+            )
+
+    return response
