f85cfc83b731342a66549f0175c50e35c8d9f1ec
Russ Garrett <russ@garrett.co.uk>
2025-03-24 08:47:12 +0000

    Container for running SQL jobs

---
+++ schema/jobs/README.md
@@ -0,0 +1,1 @@
+A container to run periodic SQL jobs
--- schema/stats.sql
+++ schema/stats.sql
@@ -27,34 +27,3 @@ CREATE TABLE stats.power_generator (
 	count INTEGER,
 	output BIGINT
 );
-
-
------------------------------
--- Queries used to update the stats schema:
-
-INSERT INTO stats.power_line (time, country, voltage, length)
-	SELECT now() AS time, country_eez_sub."union" AS country, convert_voltage(voltage)::INTEGER AS voltage,
-		sum(ST_Length(ST_Transform(geometry, 4326)::geography)) AS length
-	FROM osm_power_line, countries.country_eez_sub
-	WHERE ST_Contains(country_eez_sub.geom, geometry)
-		AND "union" != 'Antarctica'
-		AND tags->'construction:power' IS NULL
-	GROUP BY country_eez_sub."union", convert_voltage(voltage);
-
-
-INSERT INTO stats.power_plant (time, country, source, count, output)
-	SELECT now() AS time, country_eez_sub."union", source, count(*), convert_power(output)
-	FROM power_plant, countries.country_eez_sub
-	WHERE ST_Contains(country_eez_sub.geom, geometry)
-		AND "union" != 'Antarctica'
-		AND tags->'construction:power' IS NULL
-	GROUP BY country_eez_sub."union", source, convert_power(output);
-
-
-INSERT INTO stats.power_generator (time, country, source, count, output)
-	SELECT now() AS time, country_eez_sub."union", source, count(*), convert_power(output)
-	FROM osm_power_generator, countries.country_eez_sub
-	WHERE ST_Contains(country_eez_sub.geom, geometry)
-		AND "union" != 'Antarctica'
-		AND tags->'construction:power' IS NULL
-	GROUP BY country_eez_sub."union", source, convert_power(output);
