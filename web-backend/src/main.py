9e7189d86b064008badd59a9ae87fe6d3c2b179c
Russ Garrett <russ@garrett.co.uk>
2026-06-13 00:17:29 +0100

    Always rollback

--- web-backend/src/main.py
+++ web-backend/src/main.py
@@ -26,8 +26,7 @@ async def lifespan(app: Starlette) -> AsyncGenerator[State]:
         db_engine.begin() as db,
     ):
         yield {"http_client": client, "db": db}
-        if db.in_transaction():
-            await db.rollback()
+        await db.rollback()
 
 
 app = Starlette(
