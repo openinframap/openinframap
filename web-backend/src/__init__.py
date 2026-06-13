a0d7df3e3d6a61c6b9dcf747265e70a30f2d8346
Russ Garrett <russ@garrett.co.uk>
2026-06-13 18:25:37 +0100

    Backend updates, routes for new endpoints

--- web-backend/src/__init__.py
+++ web-backend/src/__init__.py
@@ -1,7 +1,8 @@
-from typing import TypedDict, cast
+from typing import Type, TypedDict, cast
 
 import httpx
 from sqlalchemy.ext.asyncio import AsyncConnection, AsyncEngine
+from starlette.exceptions import HTTPException
 from starlette.requests import Request as StarletteRequest
 
 
@@ -15,3 +16,10 @@ Request = StarletteRequest[State]
 
 def get_db(request: Request) -> AsyncConnection:
     return cast(AsyncConnection, request.scope.get("db_connection"))
+
+
+def path_param[T](request: Request, name: str, typ: Type[T]) -> T:
+    try:
+        return typ(request.path_params[name])  # type: ignore
+    except ValueError:
+        raise HTTPException(400, f"Invalid value for {name}")
--- web-backend/src/views/api.py
+++ web-backend/src/views/api.py
@@ -1,53 +1,71 @@
 from typing import Any
 
+from numpy import number
 from pydantic import BaseModel
+from shapely import from_wkt
+from sqlalchemy.dialects.postgresql import HSTORE
 from sqlalchemy.sql import text
 from starlette.exceptions import HTTPException
 from starlette.responses import Response
 from starlette.routing import Route
 
-from .. import Request, get_db
+from .. import Request, get_db, path_param
 from ..util import cache_for
+
+
+def extract_local_names(tags: dict[str, str]) -> dict[str, str]:
+    result = {}
+    for key in tags:
+        if key.startswith("name:"):
+            _, code = key.split(":", 1)
+            result[code] = tags[key]
+    return result
 
 
 class Circuit(BaseModel):
     id: int
     name: str | None
-    name_en: str | None
+    local_names: dict[str, str]
     voltage: int | None
     frequency: int | None
+
+
+class CircuitMembership(BaseModel):
+    circuit: Circuit
     role: str | None
 
 
 class SubstationResponse(BaseModel):
-    circuits: list[Circuit]
+    circuits: list[CircuitMembership]
 
 
 @cache_for(600)
 async def substation(request: Request) -> Response:
     database = get_db(request)
-    substation_id = request.path_params["substation_id"]
+    substation_id = path_param(request, "substation_id", int)
     res = await database.execute(
         text(
             """SELECT relation.osm_id AS relation_id,
             relation.tags -> 'name' AS name,
-            relation.tags -> 'name:en' AS name_en,
             relation.tags -> 'frequency' AS frequency,
+            relation.tags,
             convert_voltage(relation.tags -> 'voltage') AS voltage,
             member.role
             FROM osm_power_circuit_relation_member member, osm_power_circuit_relation relation
             WHERE relation.osm_id = member.osm_id AND member.member = :member_id"""
-        ),
-        {"member_id": int(substation_id)},
+        ).columns(tags=HSTORE),
+        {"member_id": substation_id},
     )
 
     circuits = [
-        Circuit(
-            id=r._mapping["relation_id"],
-            name=r._mapping["name"],
-            name_en=r._mapping["name_en"],
-            voltage=r._mapping["voltage"] / 1000 if r._mapping["voltage"] else None,
-            frequency=r._mapping["frequency"],
+        CircuitMembership(
+            circuit=Circuit(
+                id=r._mapping["relation_id"],
+                name=r._mapping["name"],
+                local_names=extract_local_names(r._mapping["tags"]),
+                voltage=r._mapping["voltage"] / 1000 if r._mapping["voltage"] else None,
+                frequency=r._mapping["frequency"],
+            ),
             role=r._mapping["role"],
         )
         for r in res
@@ -56,28 +74,36 @@ async def substation(request: Request) -> Response:
     return Response(SubstationResponse(circuits=circuits).model_dump_json(), media_type="application/json")
 
 
-class CircuitResponse(BaseModel):
+class CircuitSubstation(BaseModel):
     id: int
     name: str | None
-    name_en: str | None
-    voltage: int | None
-    frequency: int | None
-    length: float
+    local_names: dict[str, str]
+    location: tuple[float, float]
 
-    geometry: dict[str, Any]
+
+class CircuitResponse(Circuit):
+    length: float
+    type: str
+    wikidata: str | None
+
+    substations: list[CircuitSubstation]
+
+    geometry: dict[str, Any]  # GeoJSON
 
 
 @cache_for(600)
 async def circuit(request: Request) -> Response:
     database = get_db(request)
-    circuit_id = request.path_params["circuit_id"]
-    res = (
+    circuit_id = path_param(request, "circuit_id", int)
+    relation = (
         await database.execute(
             text("""
                 SELECT relation.osm_id AS relation_id,
                 relation.tags -> 'name' AS name,
-                relation.tags -> 'name:en' AS name_en,
                 relation.tags -> 'frequency' AS frequency,
+                relation.tags -> 'type' AS type,
+                relation.tags -> 'wikidata' AS wikidata,
+                relation.tags,
                 convert_voltage(relation.tags -> 'voltage') AS voltage,
                 (SELECT json_build_object('type', 'FeatureCollection', 'features',
                     json_agg(json_build_object('type', 'Feature',
@@ -86,47 +112,78 @@ async def circuit(request: Request) -> Response:
                     FROM osm_power_circuit_relation_member WHERE osm_id = relation.osm_id) AS geojson,
                 (SELECT sum(st_length(ST_Transform(geometry, 4326)::geography))
                     from osm_power_circuit_relation_member WHERE role = 'section' AND
-                    osm_id = relation.osm_id) AS length
+                    osm_id = relation.osm_id) AS length,
+                (select ST_AsText(st_envelope(st_collect(st_transform(geometry, 4326)))) from
+                    osm_power_circuit_relation_member where osm_id = relation.osm_id) AS bbox
 
                 FROM osm_power_circuit_relation relation
                 WHERE relation.osm_id = :relation_id
-            """),
-            {"relation_id": int(circuit_id)},
+                AND relation.tags -> 'type' IN ('power', 'route')
+            """).columns(tags=HSTORE),
+            {"relation_id": circuit_id},
         )
     ).fetchone()
-    if not res:
+    if not relation:
         raise HTTPException(404)
+
+    geojson = relation._mapping["geojson"]
+
+    bbox = from_wkt(relation._mapping["bbox"])
+    geojson["bbox"] = bbox.bounds
+
+    substations = await database.execute(
+        text("""
+            SELECT id, tags -> 'name' AS name, tags,
+                ST_AsText(ST_Transform(ST_Centroid(geometry),4326)) AS location
+            FROM osm_power_circuit_relation_member
+            WHERE osm_id = :relation_id AND role = 'substation'
+        """).columns(tags=HSTORE),
+        {"relation_id": circuit_id},
+    )
+
+    circuit_substations = [
+        CircuitSubstation(
+            id=s._mapping["id"],
+            name=s._mapping["name"],
+            local_names=extract_local_names(s._mapping["tags"]),
+            location=from_wkt(s._mapping["location"]).coords[0],
+        )
+        for s in substations
+    ]
 
     return Response(
         CircuitResponse(
-            id=res._mapping["relation_id"],
-            name=res._mapping["name"],
-            name_en=res._mapping["name_en"],
-            voltage=res._mapping["voltage"] / 1000 if res._mapping["voltage"] else None,
-            frequency=res._mapping["frequency"],
-            geometry=res._mapping["geojson"],
-            length=round((res._mapping["length"] or 0) / 1000, 2),
+            id=relation._mapping["relation_id"],
+            name=relation._mapping["name"],
+            local_names=extract_local_names(relation._mapping["tags"]),
+            voltage=relation._mapping["voltage"] / 1000 if relation._mapping["voltage"] else None,
+            frequency=relation._mapping["frequency"],
+            geometry=geojson,
+            length=round((relation._mapping["length"] or 0) / 1000, 2),
+            substations=circuit_substations,
+            type=relation._mapping["type"],
+            wikidata=relation._mapping["wikidata"],
         ).model_dump_json(),
         media_type="application/json",
     )
 
 
 class LineResponse(BaseModel):
-    circuits: list[Circuit]
+    circuits: list[CircuitMembership]
 
 
 @cache_for(600)
 async def line(request: Request) -> Response:
     database = get_db(request)
-    line_id = request.path_params["line_id"]
+    line_id = path_param(request, "line_id", int)
 
     res = await database.execute(
         text(
             """
                 SELECT relation.osm_id AS relation_id,
                     relation.tags->'name' AS name,
-                    relation.tags->'name:en' AS name_en,
                     relation.tags -> 'frequency' AS frequency,
+                    relation.tags AS tags,
                     convert_voltage(relation.tags -> 'voltage') AS voltage,
                     member.role
                 FROM osm_power_circuit_relation_member member,
@@ -135,16 +192,18 @@ async def line(request: Request) -> Response:
                 AND member.osm_id = relation.osm_id
             """
         ),
-        {"line_id": int(line_id)},
+        {"line_id": line_id},
     )
 
     circuits = [
-        Circuit(
-            id=r._mapping["relation_id"],
-            name=r._mapping["name"],
-            name_en=r._mapping["name_en"],
-            voltage=r._mapping["voltage"] / 1000 if r._mapping["voltage"] else None,
-            frequency=r._mapping["frequency"],
+        CircuitMembership(
+            circuit=Circuit(
+                id=r._mapping["relation_id"],
+                name=r._mapping["name"],
+                local_names=extract_local_names(r._mapping["tags"]),
+                voltage=r._mapping["voltage"] / 1000 if r._mapping["voltage"] else None,
+                frequency=r._mapping["frequency"],
+            ),
             role=r._mapping["role"],
         )
         for r in res
