39a08dc3c8490d7425b22db4d875802da39e0f26
Russ Garrett <russ@garrett.co.uk>
2026-09-13 15:53:20 +0100

    Ignore list

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -37,7 +37,8 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        navigateFallbackDenylist: [/^\/stats/, /^\/map/, /^\/fonts/]
       },
       manifest: {
         name: 'Open Infrastructure Map',
