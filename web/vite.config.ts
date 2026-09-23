79bcfa4929e9c0beb8adf621b1879ce0db0d8fa0
Russ Garrett <russ@garrett.co.uk>
2026-09-23 11:03:46 +0100

    Exclude copyright page

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -46,7 +46,7 @@ export default defineConfig({
       registerType: 'autoUpdate',
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-        navigateFallbackDenylist: [/^\/stats/, /^\/map/, /^\/fonts/, /^\/static/, /^\/about/]
+        navigateFallbackDenylist: [/^\/stats/, /^\/map/, /^\/fonts/, /^\/static/, /^\/about/, /^\/copyright/]
       },
       manifest: {
         name: 'Open Infrastructure Map',
